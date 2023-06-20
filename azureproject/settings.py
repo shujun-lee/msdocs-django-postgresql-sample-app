2045d5c7b066acdbfe1fa917463ab568b7ba4360
shujunn.lee <shujunn.lee@gmail.com>
2023-06-20 10:37:13 +0800

    edit create resturant

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -94,7 +94,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DBNAME'),
-        'HOST': os.environ.get('DBHOST'),
+        'HOST': 'localhost',
+        # 'HOST': os.environ.get('DBHOST'),
         'USER': os.environ.get('DBUSER'),
         'PASSWORD': os.environ.get('DBPASS'),
     }
